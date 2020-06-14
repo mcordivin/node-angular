ae63c85a6b2287ac3cceb1058aef9c1b8d7b7912
marin_cordivin <mcordivin@users.noreply.github.com>
2020-06-14 13:47:59 -0700

    angular app complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'AngularClient';
+  isAuthenticated: boolean;
+
+  async ngOnInit() {
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+  }
+
+  constructor(public oktaAuth: OktaAuthService){
+    this.oktaAuth.$authenticationState.subscribe(
+      (isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated
+    );
+  }
+
+
+  login(){
+    this.oktaAuth.loginRedirect();
+  }
+
+  logout(){
+    this.oktaAuth.logout('/');
+  }
 }
